30f26235f5d23fb683701824859b46c48a7f8b9f
Lisa Lin <linlisa0601@gmail.com>
2021-02-05 11:34:36 +0800

    complete efficientnetmodify class and let  efficientnetmodify  can use the trainer from huggingface transformers

--- src/lisa/model/efficientnet.py
+++ src/lisa/model/efficientnet.py
@@ -1,10 +1,40 @@
+import torch
+from dataclasses import dataclass
 from efficientnet_pytorch import EfficientNet
+from torch.nn import CrossEntropyLoss
+from transformers.file_utils import ModelOutput
 
-class clasify_efficientnet(EfficientNet):
-    def __init__(self, config):
-        super().__init__()
-        self.classify_num = config.classify_num
 
-    def forward(self):
-        pass
+@dataclass
+class EfficientNetOutput(ModelOutput):
+    loss: torch.FloatTensor = None
+    logits: torch.FloatTensor = None
+    features: torch.FloatTensor = None
 
+
+class EfficientNetModify(EfficientNet):
+    def __init__(self, blocks_args=None, global_params=None):
+        super().__init__(blocks_args, global_params)
+
+    def forward(self,
+                inputs=None,
+                labels=None):
+        features = super().forward(inputs)
+
+        if self._global_params.include_top:
+            logits = features
+            features = None
+        else:
+            x = features.flatten(start_dim=1)
+            x = self._dropout(x)
+            logits = self._fc(x)
+        loss = None
+        if len(labels) > 0:
+            loss_fun = CrossEntropyLoss()
+            loss = loss_fun(logits, labels - 1)
+
+        return EfficientNetOutput(
+            loss=loss,
+            logits=logits,
+            features=features
+        )

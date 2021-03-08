4b70c2cfb198c0bbf8637d44206199d89259e4a4
Lisa06010416 <linlisa0601@gmail.com>
2021-03-08 21:30:09 +0800

    add metric package * add acc function

---
+++ src/lisa/model/metrics.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import accuracy_score
+
+
+def acc_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc
+    }

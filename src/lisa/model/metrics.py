2e5a81d0bbc26b52aaa3d6fb127f3cfbb0c7e231
Lisa06010416 <linlisa0601@gmail.com>
2021-03-08 21:48:58 +0800

    fix acc metrics bug

--- src/lisa/model/metrics.py
+++ src/lisa/model/metrics.py
@@ -3,7 +3,10 @@ from sklearn.metrics import accuracy_score
 
 def acc_metrics(pred):
     labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
+    try:
+        preds = pred.predictions.argmax(-1)
+    except:
+        preds = pred.predictions[0].argmax(-1)
     acc = accuracy_score(labels, preds)
     return {
         'accuracy': acc

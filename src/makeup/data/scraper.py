630f75f805a9fe2e23ae5f9709af9b543fba0efe
Lisa06010416 <linlisa0601@gmail.com>
2022-02-04 17:42:15 +0800

    rewrite scraper

--- src/makeup/data/scraper.py
+++ src/makeup/data/scraper.py
@@ -2,7 +2,7 @@ import os
 import time
 import json
 import wget
-
+import re
 import requests
 from urllib.parse import urljoin
 from selenium import webdriver
@@ -12,14 +12,31 @@ from bs4 import BeautifulSoup
 from makeup.data.utils import is_image_path, create_dir
 from makeup.utils.logmanager import get_logger
 from abc import ABC, abstractmethod
-
+from googlesearch import search
+from dataclasses import dataclass, field
+from collections import namedtuple
 
 logger = get_logger(__name__)
+
+class GoogleSearch():
+    @staticmethod
+    def search(web_url, **kwargs):
+        return search(web_url, **kwargs)
+
+switch_page_urls = namedtuple('switch_page_urls', 'oldest_page, last_page, newest_page, next_page')
+
+@dataclass
+class Article:
+    url: str
+    title: float = ""
+    images: list = field(default_factory=lambda: [])
+    text: str = ""
+    other: dict = field(default_factory=lambda: {})
 
 
 class Scraper(ABC):
     @abstractmethod
-    def get_webdata(self, web_url, **kwargs):
+    def get_webdata(self, url, **kwargs):
         pass
 
     @staticmethod
@@ -33,24 +50,22 @@ class Scraper(ABC):
             except:
                 logger.info("Download file {} fail".format(url))
 
-    def download_images_from_list(self, url_list, savepath):
+    @staticmethod
+    def download_from_urllist(url_list, savepath):
         for image_url in url_list:
-            self.download_from_url(image_url, savepath)
+            Scraper.download_from_url(image_url, savepath)
 
-
-class GoogleSearchScraper(Scraper):
-    def get_webdata(self, web_url, **kwargs):
-        from googlesearch import search
-        return search(web_url, **kwargs)
 
 
 class RequestScraper(Scraper):
     def __init__(self, **kwargs):
         pass
 
-    def get_webdata(self, web_url, **kwargs):
-        resp = requests.get(web_url)
+    def get_webdata(self, url, **kwargs):
+        resp = requests.get(url, **kwargs)
         return BeautifulSoup(resp.content, 'html.parser')
+
+
 
 
 class SeleniumScraper(Scraper):
@@ -58,8 +73,8 @@ class SeleniumScraper(Scraper):
         chromedriver = self._get_chromedriver(options, executable_path, **kwargs)
         self.chrome = chromedriver
 
-    def get_webdata(self, web_url, **kwargs):
-        self.chrome.get(web_url)
+    def get_webdata(self, url, **kwargs):
+        self.chrome.get(url)
         self.chrome.implicitly_wait(5)
         try:  # 您的連線不是私人連線
             go_button = self.chrome.find_element_by_id("details-button")
@@ -127,35 +142,70 @@ class PttScraper(RequestScraper):
             cls.__bases__ = (base,)
         return cls(**kwargs)
 
-    def get_ppt_article_list(self, soup):
-        article_anchor_info = []
+    def get_article_detail(self, article):
+        article_url = self.domain_name + article.url
+        soup = self.get_webdata(article_url)
+        article_and_command = soup.find("div", id="main-container")
+
+        # article_meta info
+        article_meta_soup = article_and_command.find_all(class_=re.compile("article-meta-.*"))
+        article_meta_list = self._get_attrs_and_text(article_meta_soup)
+        article_meta = {}
+        for idx in range(0, len(article_meta_list), 2):
+            article_meta[article_meta_list[idx]['text']] = article_meta_list[idx + 1]['text']
+        article.other["article_meta"] = article_meta
+        article.title = article_meta["標題"]
+
+        # push
+        push_soup = article_and_command.find_all("span", class_=re.compile("push.*"))
+        push_list = self._get_attrs_and_text(push_soup)
+        push_idx = 0
+        push_info_list = []
+        while push_idx < len(push_list):
+            if "push-tag" in push_list[push_idx]['class']:
+                push_info_list.append(
+                    {
+                        "push-tag": push_list[push_idx]['text'],
+                        "push-userid": push_list[push_idx + 1]['text'],
+                        "push-conten": push_list[push_idx + 2]['text'],
+                        "push-ipdatetime": push_list[push_idx + 3]['text']
+                    }
+                )
+                push_idx += 4
+            else:
+                push_idx += 1
+
+        # get image path
+        image_soup = article_and_command.find_all("a")
+        image_soup_list = self._get_attrs_and_text(image_soup)
+        for index, info in enumerate(image_soup_list):
+            href = info['href']
+            if is_image_path(href):
+                article.images.append(href)
+
+        # text
+        text = re.sub("(<.*>)|(<span.*)|(\">[./: \t\d+]+)|(※.*)|(--\n)", "", str(article_and_command))
+        article.text = "\n".join([line for line in text.split("\n") if line.strip()])
+
+    def get_articlelist_and_switchurls_from_contenturl(self, url):
+        soup = self.get_webdata(url)
+
+        # gat articles in content url
+        article_list = []
         resultset = soup.find_all("div", class_='title')
         for result in resultset:
             article_anchor = result.find('a')
-            info = self.get_attrs_and_text(article_anchor)
-            article_anchor_info.append(info[0])
-        return article_anchor_info
+            info = self._get_attrs_and_text(article_anchor)[0]
+            article = Article(url=info['href'], title=info['text'])
+            article_list.append(article)
 
-    def _get_imageurl_from_article(self, article):
-        # article_url and get soup
-        article_url = self.domain_name + article['href']
-        soup = self.get_webdata(article_url)
+        # get urls used for switch pages
+        switch_urls = self._get_change_contens_url(soup)
+        return article_list, switch_urls
 
-        # parser
-        article_and_command = soup.find("div", id="main-container")
-        content = article_and_command.find_all("a")
-        content_info = self.get_attrs_and_text(content)
-
-        # get image path
-        article['images'] = []
-        for index, info in enumerate(content_info):
-            href = info['href']
-            if is_image_path(href):
-                article['images'].append(href)
-
-    def _get_change_contens_button(self, soup):
+    def _get_change_contens_url(self, soup):
         button_info = soup.find_all("a", class_="btn wide")
-        button_info = self.get_attrs_and_text(button_info)
+        button_info = self._get_attrs_and_text(button_info)
         newest_page = None
         oldest_page = None
         last_page = None
@@ -169,34 +219,32 @@ class PttScraper(RequestScraper):
                 newest_page = button['href']
             elif "下頁" in button['text']:
                 next_page = button['href']
-        return oldest_page, last_page, newest_page, next_page
+        return switch_page_urls(oldest_page=oldest_page,
+                                last_page=last_page,
+                                newest_page=newest_page,
+                                next_page=next_page)
 
-    def scraper_image(self, savepath, search_page_num=1, keyword=None, search_by_keyword=False):
-        """
-        還未加入keyword search
-        """
-        if search_by_keyword:
-            contents_url = 'https://www.ptt.cc/bbs/movie/search?'
-        else:
-            contents_url = self.board_url
-        articles = None
+    def get_articles(self, keyword=None, url=None, search_page_num=1):
+        if not url:
+            if keyword:
+                url = urljoin(self.board_url, f"search?q={keyword}")
+            else:
+                url = self.board_url
+
         for _ in range(search_page_num):
-            soup = self.get_webdata(contents_url)
-            articles = self.get_ppt_article_list(soup)
-            for article in articles:
-                # get image path
-                if 'href' in article:
-                    self._get_imageurl_from_article(article)
-                    if 'images' in article:
-                        self.download_images_from_list(article['images'], savepath)
-                time.sleep(1)
-            _, last_page, _, _ = self._get_change_contens_button(soup)
-            contents_url = urljoin(self.domain_name, last_page)
-        return articles
+            articles, switch_urls = self.get_articlelist_and_switchurls_from_contenturl(url)
+            yield articles
+            url = urljoin(self.domain_name, switch_urls.next_page)
 
+    def download_images(self, savepath, search_page_num=1):
+        articles = self.get_articles(search_page_num)
+        for _ in range(search_page_num):
+            for article in next(articles):
+                self.get_article_detail(article)
+                self.download_from_urllist(article.images, savepath)
 
     @staticmethod
-    def get_attrs_and_text(resultset):
+    def _get_attrs_and_text(resultset):
         all_info = []
         if isinstance(resultset, Sequence):
             for r in resultset:
@@ -225,7 +273,7 @@ class XiaohongshuScraper(SeleniumScraper):
         self.image_w = w
         self.image_h = h
 
-    def scraper(self, web_url):
+    def download_images(self, web_url):
         soup = self.get_webdata(web_url)
         # get image
         result = soup.find_all("i", style=lambda style: style and "background-image" in style)
@@ -237,7 +285,6 @@ class XiaohongshuScraper(SeleniumScraper):
             url = url.split(');')[0]
             url = url if 'https' in url else "https:" + url
             urls.append(url)
-        print(urls)
         return urls
 
 
@@ -248,7 +295,7 @@ class DcardScraper(SeleniumScraper):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def get_urlpath(self, soup):
+    def get_img_urlpath(self, soup):
         urls = []
         for i in str(soup).split('"'):
             if is_image_path(i):
@@ -263,20 +310,28 @@ class DcardScraper(SeleniumScraper):
             cls.__bases__ = (base,)
         return cls(**kwargs)
 
-    def scraper(self, keyword, savepath, limit=3, search_by="keyword"):
-        search_path = ""
-        if search_by == "keyword":
-            search_path = "https://www.dcard.tw/service/api/v2/search/posts?limit={}&query={}".format(limit,keyword)
-        elif search_by == "keyword":
-            search_path = "{}".format(keyword)
+    def get_articles(self, keyword, limit=3, search_by="keyword"):
+        search_path = "https://www.dcard.tw/service/api/v2/search/posts?limit={}&query={}".format(limit, keyword)
         soup = self.get_webdata(search_path)
-
-        articles = json.loads(soup.text)
-        for article in articles:
-            article_id = article['id']
-            article_url = "https://www.dcard.tw/service/api/v2/posts/{}".format(article_id)
-            soup = self.get_webdata(article_url)
-            url = self.get_urlpath(soup)
-            article['images'] = url
-            self.download_images_from_list(article['images'], savepath)
+        articles = []
+        for info in json.loads(soup.text):
+            article = Article(url=str(info["id"]))
+            articles.append(article)
         return articles
+
+    def get_article_detail(self, article):
+        article_url = "https://www.dcard.tw/service/api/v2/posts/{}".format(article.url)
+        soup = self.get_webdata(article_url)
+        article.images = self.get_img_urlpath(soup)
+        json_data = json.loads(self.get_webdata(article_url).text)
+        article.title = json_data["title"]
+        article.text = json_data["excerpt"]
+        del json_data["id"], json_data["title"], json_data["excerpt"]
+        article.other = json_data
+
+    def download_images(self, keyword, savepath, limit=3):
+        articles = self.get_articles(keyword, limit)
+
+        for article in articles:
+            self.get_article_detail(article)
+            self.download_from_urllist(article.images, savepath)
--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+import time
+import pytest
 from bs4 import BeautifulSoup
 
-from makeup.data.scraper import RequestScraper, SeleniumScraper, PttScraper, XiaohongshuScraper, DcardScraper
+from makeup.data.scraper import RequestScraper, SeleniumScraper, PttScraper, XiaohongshuScraper, DcardScraper, Article
 from makeup.utils.envchecker import get_chrome_driver
 from makeup.script import setup_mitmdump_server, close_mitmdump_server
 
@@ -14,9 +16,6 @@ test_img_url = "https://drive.google.com/u/1/uc?id=1Y_6wLpMBscr1Kkw13xDr5kKgkrEy
 test_xiaohongshu_url = "https://www.xiaohongshu.com/discovery/item/5ff15da0000000000101ef28?source=question"
 
 get_chrome_driver()
-
-# open mitmdump_server
-setup_mitmdump_server()
 
 
 def get_soup_from_file(path):
@@ -49,7 +48,7 @@ class TestRequestScraper:
         if os.path.isdir(savepath):
             shutil.rmtree(savepath)
         scraper = RequestScraper()
-        scraper.download_images_from_list([self.test_img_url]*3, savepath)
+        scraper.download_from_urllist([self.test_img_url]*3, savepath)
         assert os.path.isdir(savepath), "Didn't creat dir!"
         assert len(os.listdir()) != 3, "download from url error!"
 
@@ -69,9 +68,12 @@ class TestSeleniumScraper:
         assert soup
 
     def test_get_webdata_with_proxy(self):
+        setup_mitmdump_server()
+        time.sleep(3)
         scraper = SeleniumScraper(executable_path=chromedriver_path, use_proxy=True)
         soup = scraper.get_webdata(self.test_web_url)
         assert soup
+        close_mitmdump_server()
 
 
 class TestPttScraper:
@@ -82,65 +84,59 @@ class TestPttScraper:
 
     def test_get_scraper(self):
         # test get scraper
-        scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
-                                         base=RequestScraper)
-        assert scraper
+        scraper = PttScraper(domain_name="https://www.ptt.cc/",
+                             board_url="https://www.ptt.cc/bbs/MakeUp/")
         assert isinstance(scraper, RequestScraper)
         soup = scraper.get_webdata(self.test_web_url)
         assert soup
 
     def test_get_scraper_change_base(self):
-        # change  father class
+        print(chromedriver_path)
         scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
+                                         board_url="https://www.ptt.cc/bbs/MakeUp/",
                                          executable_path=chromedriver_path,
                                          base=SeleniumScraper)  # 換過了該class就會指定為SeleniumScraper
-        assert scraper
         assert isinstance(scraper, SeleniumScraper)
         soup = scraper.get_webdata(self.test_web_url)
         assert soup
 
-    def test_get_ppt_article_list(self):
-        # load test data
+    def test_get_articles(self):
         scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
+                                         board_url="https://www.ptt.cc/bbs/MakeUp/",
                                          base=RequestScraper)
 
-        article_list = scraper.get_ppt_article_list(self.ptt_board_soup)
-        assert article_list
-        assert len(article_list)==23
+        # 用keyword search
+        article_generator = scraper.get_articles(keyword="粉餅")
+        assert next(article_generator)
+        with pytest.raises(StopIteration):
+            next(article_generator)
 
-    def test_get_change_content_button(self):
-        scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
-                                         base=RequestScraper)
-        oldest_page, last_page, newest_page, next_page = scraper._get_change_contens_button(self.ptt_board_soup)
-        assert oldest_page=="/bbs/MakeUp/index1.html"
-        assert last_page == "/bbs/MakeUp/index3626.html"
-        assert newest_page == "/bbs/MakeUp/index.html"
-        assert not next_page
+        # 指定目錄的url
+        article_generator = scraper.get_articles(url="https://www.ptt.cc/bbs/MakeUp/index3723.html")
+        assert next(article_generator)
+        with pytest.raises(StopIteration):
+            next(article_generator)
 
-    def test_get_imageurl_from_article(self):
-        article = {'href': '/bbs/MakeUp/M.1616415945.A.11C.html', 'text': '[心得] Addiction新品/夏季限定 (靠櫃產品心得)'}
+    def test_get_article_detail(self):
+        article = Article(url='/bbs/MakeUp/M.1616415945.A.11C.html')
         scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
+                                         board_url="https://www.ptt.cc/bbs/MakeUp/",
                                          base=RequestScraper)
-        scraper._get_imageurl_from_article(article)
-        assert "images" in article
-        assert len(article["images"]) == 12
+        scraper.get_article_detail(article)
+        assert len(article.images) == 12
 
-    def test_scraper_image(self):
+    def test_download_images(self):
         scraper = PttScraper.get_scraper(domain_name="https://www.ptt.cc/",
-                                         board_url="https://www.ptt.cc/bbs/MakeUp/index.html",
+                                         board_url="https://www.ptt.cc/bbs/MakeUp/",
                                          base=RequestScraper)
-        articles = scraper.scraper_image(savepath="testdata/image/")
-        assert articles
+        scraper.download_images(savepath="testdata/image/", search_page_num=1)
 
 
 class TestXiaohongshuScraper:
     def setup(self):
         self.test_web_url = test_xiaohongshu_url
+        setup_mitmdump_server()
+        time.sleep(3)
 
     def test_set_imagesize(self):
         scraper = XiaohongshuScraper(executable_path=chromedriver_path,
@@ -152,9 +148,13 @@ class TestXiaohongshuScraper:
     def test_scraper(self):  # error
         scraper = XiaohongshuScraper(executable_path=chromedriver_path,
                                      use_proxy=True)
-        urls = scraper.scraper(self.test_web_url)
+        urls = scraper.download_images(self.test_web_url)
         print(urls)
         pass
+
+    def teardown(self):
+        close_mitmdump_server()
+
 
 
 class TestDcardScraper:
@@ -162,20 +162,28 @@ class TestDcardScraper:
         self.test_web_url = test_web_url
         self.dcard_article_soup = get_soup_from_file("testdata/dcard_article")
 
-    def test_get_urlpath(self):
+    def test_get_img_urlpath(self):
         scraper = DcardScraper.get_scraper(executable_path=chromedriver_path)
-        url = scraper.get_urlpath(self.dcard_article_soup)
-        assert url
-        assert len(url) == 176
+        url = scraper.get_img_urlpath(self.dcard_article_soup)
+        assert len(url) == 150
 
-    def test_scraper(self):
+    def test_download_images(self):
         scraper = DcardScraper.get_scraper(executable_path=chromedriver_path)
-        articles = scraper.scraper(keyword="口紅", savepath="testdata/image/", limit=1)
-        assert articles
+        scraper.download_images(keyword="口紅", savepath="testdata/image/", limit=1)
+        assert os.listdir("testdata/image/")
+
+    def test_get_articles(self):
+        scraper = DcardScraper.get_scraper(executable_path=chromedriver_path)
+        articles = scraper.get_articles("口紅", limit=2)
+        assert len(articles)==2
+
+    def test_get_article_detail(self):
+        scraper = DcardScraper.get_scraper(executable_path=chromedriver_path)
+        article = Article(url="238038505")
+        scraper.get_article_detail(article)
+        assert article.title
+        assert article.text
 
     def teardown(self):
         if os.path.isdir("testdata/image/"):
             shutil.rmtree("testdata/image/")
-
-
-close_mitmdump_server()
